9fb969dec459791534e9552f313512a74c7538b8
Brian Ketelsen <bketelsen@gmail.com>
2016-11-22 20:18:18 -0500

    changes from last class

--- src/contexts/solutions/contextrpc/client/client.go
+++ src/contexts/solutions/contextrpc/client/client.go
@@ -57,7 +57,9 @@ func httpDo(ctx context.Context, req *http.Request, f func(*http.Response, error
 	tr := &http.Transport{}
 	client := &http.Client{Transport: tr}
 	c := make(chan error, 1)
-	go func() { c <- f(client.Do(req)) }()
+	go func() {
+		c <- f(client.Do(req))
+	}()
 	select {
 	case <-ctx.Done():
 		tr.CancelRequest(req)
--- src/wranglingconcurrency/demos/race/main.go
+++ src/wranglingconcurrency/demos/race/main.go
@@ -1,24 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 func main() {
 
-	var mu sync.Mutex
+	//	var mu sync.Mutex
 	c := make(chan bool)
-	m := make(map[string]string)
-	go func() {
-		mu.Lock()
-		m["1"] = "a" // First conflicting access.
-		mu.Unlock()
-		c <- true
-	}()
-	mu.Lock()
-	m["2"] = "b" // Second conflicting access.
-	mu.Unlock()
+	m := make(map[int]string)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			//		mu.Lock()
+			m[i] = "a" // First conflicting access.
+			//		mu.Unlock()
+			c <- true
+		}()
+	}
+	//	mu.Lock()
+	m[2] = "b" // Second conflicting access.
+	//	mu.Unlock()
 	<-c
 	for k, v := range m {
 		fmt.Println(k, v)
